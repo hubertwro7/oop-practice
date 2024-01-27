cfe72d8946e015768a6a13288488e753bd5b23c7
hubertwro7 <hubertwro7@gmail.com>
2024-01-27 17:29:52 +0100

    finished zad1

---
+++ zad1/Licz.cs
@@ -0,0 +1,23 @@
+﻿namespace zad1
+{
+    internal class Licz
+    {
+        private int wartosc;
+        public Licz(int wartosc)
+        {
+            this.wartosc = wartosc;
+        }
+        public void Dodaj(int liczba)
+        {
+            wartosc += liczba;
+        }
+        public void Odejmij(int liczba)
+        {
+            wartosc -= liczba;
+        }
+        public void WypiszWartosc()
+        {
+            Console.WriteLine($"Wartosc: {wartosc}");
+        }
+    }
+}
---
+++ zad1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Licz licz1 = new Licz(2);
+            licz1.Odejmij(2);
+            licz1.Dodaj(1);
+            licz1.WypiszWartosc();
+        }
+    }
+}
