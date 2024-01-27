bed7a7ad588dd422a415c4f825b7572ae74425cf
hubertwro7 <hubertwro7@gmail.com>
2024-01-27 18:09:17 +0100

    finished zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liczby1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            Sumator sumator1 = new Sumator(liczby1);
+            sumator1.WypiszLiczby();
+            sumator1.WypiszWPrzedziale(-1, 6);
+            sumator1.WypiszWPrzedziale(2, 3);
+            Console.WriteLine($"Ile elementow: {sumator1.IleElementow()}");
+            Console.WriteLine($"Suma podzielnych przez 3: {sumator1.SumaPodziel3()}");
+            Console.WriteLine($"Suma: {sumator1.Suma()}");
+        }
+    }
+}
---
+++ zad2/Sumator.cs
@@ -0,0 +1,62 @@
+﻿namespace zad2
+{
+    internal class Sumator
+    {
+        private int[] liczby;
+        public Sumator(int[] liczby)
+        {
+            this.liczby = liczby;   
+        }
+        public int Suma()
+        {
+            int suma = 0;
+            foreach(int i in liczby)
+            {
+                suma += i;
+            }
+            return suma;
+        }
+        public int SumaPodziel3()
+        {
+            int suma = 0;
+            foreach(int i in liczby) 
+            {
+                if(i % 3 == 0)
+                {
+                    suma += i;
+                }
+            }
+            return suma;
+        }
+        public int IleElementow()
+        {
+            int licznik = 0;
+            foreach(int i in liczby)
+            {
+                licznik++;
+            }
+            return licznik;
+        }
+        public void WypiszLiczby()
+        {
+            Console.WriteLine("Liczby:");
+            foreach (int i in liczby)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+        public void WypiszWPrzedziale(int lowIndex, int highIndex) 
+        {
+            Console.WriteLine("Liczby w przedziale:");
+            for(int i = lowIndex; i <= highIndex; i++)
+            {
+                if(i >= 0 && i < IleElementow())
+                {
+                    Console.Write(liczby[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
