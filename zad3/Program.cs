8297b6da318dc778eb2febcda6f2311302b0ea72
hubertwro7 <hubertwro7@gmail.com>
2024-01-28 16:18:30 +0100

    finished zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Statyczna s1 = new Statyczna(1);
+            Console.WriteLine(Statyczna.Liczba);
+            Statyczna.Zwieksz();
+            Statyczna.Zwieksz();
+            Statyczna.Zwieksz();
+            Console.WriteLine(Statyczna.Liczba);
+        }
+    }
+}
---
+++ zad3/Statyczna.cs
@@ -0,0 +1,17 @@
+﻿namespace zad3
+{
+    internal class Statyczna
+    {
+        int I;
+        public static int Liczba;
+        public static void Zwieksz()
+        {
+            Liczba++;
+        }
+        public Statyczna(int I)
+        {
+            this.I = I;
+            Zwieksz();
+        }
+    }
+}
