2163960184a95c86aa1a83f36e2fd278525cab5f
hubertwro7 <hubertwro7@gmail.com>
2024-02-01 23:52:14 +0100

    zad5 almost done instructions unclear

---
+++ zad5/Liczba.cs
@@ -0,0 +1,51 @@
+﻿namespace zad5
+{
+    internal class Liczba
+    {
+        private readonly char number;
+        private int[] decimalPart = Array.Empty<int>();
+        public Liczba(string wholeNumber)
+        {
+            if (wholeNumber.Contains('.'))
+            {
+                var substring = wholeNumber.Substring(wholeNumber.IndexOf('.')+1);
+                decimalPart = new int[substring.Length];
+                for(int i = 0; i < decimalPart.Length; i++)
+                {
+                    decimalPart[i] = Int32.Parse(substring[i].ToString());
+                }
+            }
+
+            this.number = wholeNumber[0];
+        }
+        public string WholeNumber()
+        {
+            string decimalPartTemp = "";
+            foreach (int value in decimalPart)
+            {
+                decimalPartTemp += value.ToString();
+            }
+            if (decimalPart.Length != 0)
+            {
+                
+                return $"{number}.{decimalPartTemp}";
+            }
+            return $"{number}";
+        }
+        public void Display()
+        {
+            Console.WriteLine(WholeNumber());
+        }
+
+        public int Factorial(int value)
+        {
+            var factorial = 1;  
+            for(int i = 1; i <= value; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;   
+        }
+    }
+}
+ 
---
+++ zad5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace zad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var liczba = new Liczba("3.14152");
+            liczba.Display();
+            Console.WriteLine(liczba.Factorial(3));
+        }
+    }
+}
